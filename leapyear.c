991f65607bd318ff93baa4b389e18c565d2f0449
Raushan Kumar Singh <raushanksingh10@gmail.com>
2026-08-12 21:51:31 +0530

    Add leap year program

---
+++ leapyear.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int year;
+    printf("Enter the year:");
+    scanf("%d",&year);
+    if(year%400==0)
+    {
+        printf("It is a leap year");
+    }
+    else if(year%100==0)
+    {
+        printf("It is not a leap year");
+    }
+    else if(year%4==0)
+    {
+        printf("It is a leap year");
+    }
+    else
+    {
+        printf("it is not a leap year");
+    }
+    return 0;
+}
