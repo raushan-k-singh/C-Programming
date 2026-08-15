8a90fdd08591cb1a264601429b9719dc736efdd9
Raushan Kumar Singh <raushanksingh10@gmail.com>
2026-08-15 21:18:57 +0530

    Add multiple of 2 and 3 program

---
+++ multipleof2and3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    if(num % 2 == 0 && num % 3 == 0)
+    {
+        printf("The number is divisible by 2 and 3");
+    }
+    else if(num % 2 == 0)
+    {
+        printf("The number is divisible by 2");
+    }
+    else if(num % 3 == 0)
+    {
+        printf("The number is divisible by 3");
+    }
+    else 
+    {
+        printf("The number is not divisible by 2 and 3");
+    }
+    return 0;
+}
